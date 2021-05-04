41a2fa66a3ae96326cbda561467b5e96345b1b7f
mathvt <mathvboite@mail.com>
2021-05-04 18:53:06 +0200

    step1

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,23 @@
 
-function wait(){
-    setTimeout(function(){
-        let dir = document.getElementById('name').value
-        window.location.href = '/' + dir       
-    },500)    
+function goBack(){
+    window.history.back()
+};
+
+
+function sendRequest(){
+    const info = {
+        main: document.getElementById('main').value,
+        syntax: document.getElementById('syntax').value,
+        expiration: document.getElementById('expiration').value,
+        name: document.getElementById('name').value
+    }
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200){
+            window.location.href = '/' + this.response
+        }
+    }
+    xhttp.open("POST", "/", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(info));
 }
-
-
-
-
-
-
-
-
-
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 let express = require('express');
 let router = express.Router();
 let fs = require('fs')
-
+const crypto = require('crypto');
+const { send } = require('process');
 
 
 router.get('/:id', function(req, res) {
-  res.sendFile('C:/Users/mathieu/Desktop/project/pastebin/public/texts' + req.originalUrl + '.html');
+  res.sendFile('C:/Users/mathieu/Desktop/project/pastebin/public/texts/' + req.originalUrl + '.html');
 });
 
 
 router.post('/', function(req, res) {
   let data = req.body
   console.log(data)
-  createHTML(data);
-  res.sendStatus(204)
+  fileName = createHTML(data);
+  res.send(fileName)
 });
 
 
@@ -26,10 +27,13 @@ module.exports = router
 function createHTML(data){
   let time = THEdate()
   let html = fs.readFileSync('./views/template.html', 'utf-8',err => console.error(err))
-  html = html.replace('replace_title', data.name)
+  let title = data.name || 'Untitled paste'
+  html = html.replace('replace_title', title)
   html = html.replace('replace_time', time)
   html = html.replace('replace_main', data.main)
-  fs.writeFileSync('./public/texts/' + data.name +'.html', html)
+  let fileName = sha1(data.main)
+  fs.writeFileSync('./public/texts/' + fileName +'.html', html)
+  return fileName
 }
 
 
@@ -37,4 +41,10 @@ function THEdate(){
   let d = new Date()
   let postDate = d.toLocaleString('default', { month: 'long' }) + ' ' + d.getDate() + 'th ' +  + d.getFullYear()
   return postDate
+}
+
+
+function sha1(input){
+  let sha1 = crypto.createHash('sha1')
+  return sha1.update(input).digest('hex');
 }
